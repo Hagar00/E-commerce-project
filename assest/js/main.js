de6f72e0f37163b66f3c79a562e7b646dfe404af
MR-Elashmawy <106967662+MR-Elashmawy@users.noreply.github.com>
2022-09-26 19:32:12 +0200

    creating Servics pags

---
+++ assest/js/main.js
@@ -0,0 +1,29 @@
+// Jquery of Restaurants Cards Show and Hide
+$("#Most_Selling").on('click', function () {
+$('#categ1').slideToggle(1000);
+});
+
+$("#Combo_Meals").on('click', function () {
+$('#categ2').slideToggle(1000);
+});
+
+$("#Value_Meals").on('click', function () {
+$('#categ3').slideToggle(1000);
+});
+
+$("#Groovy_Offers").on('click', function () {
+$('#categ4').slideToggle(1000);
+});
+
+$("#Beef_Burger_Sandwiches").on('click', function () {
+$('#categ5').slideToggle(1000);
+});
+
+$("#Chicken_Burgers").on('click', function () {
+$('#categ6').slideToggle(1000);
+});
+
+$("#Hot_Dog").on('click', function () {
+$('#categ7').slideToggle(1000);
+});
+// filtering selection functions
