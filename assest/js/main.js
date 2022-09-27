1ef60b224d13785530a97deda1047ff6e6710a01
MR-Elashmawy <106967662+MR-Elashmawy@users.noreply.github.com>
2022-09-27 11:38:50 +0200

    making filter second function on services

--- assest/js/main.js
+++ assest/js/main.js
@@ -26,7 +26,9 @@ $('#categ6').slideToggle(1000);
 $("#Hot_Dog").on('click', function () {
 $('#categ7').slideToggle(1000);
 });
-// filtering selection functions
+
+// filtering selection functions """"""""""""""""""""FIRST SOLUTION"""""""""""""""""""""
+
 if ($("#Restaurants").hover()) {
     $("#Restaurants").on('click',function(){
     $(".cardRestaurants").show();
@@ -48,15 +50,51 @@ if ($("#Markets").hover()) {
     $(".cardRestaurants").hide();
     })
 }
-// $("#Restaurants").on ('click', function () {
-//     $(".cardBakeries").hide();
-//     $(".cardMarkets").hide();
-// });
-// $("#Bakeries").on ('click', function () {
-//     $(".cardRestaurants").hide();
-//     $(".cardMarkets").hide();
-// });
-// $("#Markets").on ('click', function () {
-//     $(".cardBakeries").hide();
-//     $(".cardRestaurants").hide();
-// });
+// filtering selection functions """"""""""""""SECOND SOLUTION"""""""""""""""""""
+// filterSelection("all")
+//                 function filterSelection(c) {
+//                     var x, i;
+//                     x = document.getElementsByClassName("filterDiv");
+//                     if (c == "all") c = "";
+//                     // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+//                     for (i = 0; i < x.length; i++) {
+//                         w3RemoveClass(x[i], "show");
+//                         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//                     }
+//                 }
+
+//                 // Show filtered elements
+//                 function w3AddClass(element, name) {
+//                     var i, arr1, arr2;
+//                     arr1 = element.className.split(" ");
+//                     arr2 = name.split(" ");
+//                     for (i = 0; i < arr2.length; i++) {
+//                         if (arr1.indexOf(arr2[i]) == -1) {
+//                             element.className += " " + arr2[i];
+//                         }
+//                     }
+//                 }
+
+//                 // Hide elements that are not selected
+//                 function w3RemoveClass(element, name) {
+//                     var i, arr1, arr2;
+//                     arr1 = element.className.split(" ");
+//                     arr2 = name.split(" ");
+//                     for (i = 0; i < arr2.length; i++) {
+//                         while (arr1.indexOf(arr2[i]) > -1) {
+//                             arr1.splice(arr1.indexOf(arr2[i]), 1);
+//                         }
+//                     }
+//                     element.className = arr1.join(" ");
+//                 }
+
+//                 // Add active class to the current control button (highlight it)
+//                 var btnContainer = document.getElementById("myBtnContainer");
+//                 var btns = btnContainer.getElementsByClassName("btn");
+//                 for (var i = 0; i < btns.length; i++) {
+//                     btns[i].addEventListener("click", function () {
+//                         var current = document.getElementsByClassName("active");
+//                         current[0].className = current[0].className.replace(" active", "");
+//                         this.className += " active";
+//                     });
+//                 }
